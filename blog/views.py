c64934ed7864804db363cdc38b116ccbf18b6d1b
EwanLehrian <ewan.lehrian@gmail.com>
2020-06-10 11:28:41 +0200

    corrigés bug

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,15 @@ from blog.models import Post
 # Create your views here.
 
 def post_list(request):
-    posts= Post.objects.all()
+    #posts= Post.objects.all()
     return render(request, 'blog/post_list.html', {
-         "posts":posts
+    #     "posts":posts
     })
-from .forms import PostForm
-from django.urls import path
-from . import views
+#from django.urls import path
+#from . import views
 
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-]
+#urlpatterns = [
+#    path('', views.post_list, name='post_list'),
+#    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+#    path('post/new/', views.post_new, name='post_new'),
+#]
