f74f7c38de79e7c39c3588eea82ea3f6215825c8
EwanLehrian <ewan.lehrian@gmail.com>
2020-06-10 10:46:50 +0200

    pret pour commencer css

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts= Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+         "posts":posts
+    })
+from .forms import PostForm
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+]
